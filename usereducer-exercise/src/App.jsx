ddff47932af669614c789d2d7321be3285850f0d
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-05-01 21:05:21 +0200

    Add input

--- usereducer-exercise/src/App.jsx
+++ usereducer-exercise/src/App.jsx
@@ -1,40 +1,23 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 import "./App.css";
 
-const ACTIONS = {
-  INCREMENT: "increment",
-  DECREMENT: "decrement",
-};
+const ACTIONS = {};
 
-function reducer(state, action) {
-  switch (action.type) {
-    case ACTIONS.INCREMENT:
-      return { count: state.count + 1 };
-    case ACTIONS.DECREMENT:
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
-}
+function reducer(state, action) {}
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-
-  function increment() {
-    dispatch({ type: ACTIONS.INCREMENT });
-  }
-
-  function decrement() {
-    dispatch({ type: ACTIONS.DECREMENT });
-  }
+  const [todos, dispatch] = useReducer(reducer, []);
 
   return (
     <>
-      <button onClick={decrement}>-</button>
-      <span>{state.count}</span>
-      <button onClick={increment}>+</button>
+      <form>
+        <input type="text" value={name}></input>
+        <input type="text" value={name}></input>
+      </form>
     </>
   );
 }
 
 export default App;
+
+//gdzieś prrzy dziwiątej minucie przerwa
