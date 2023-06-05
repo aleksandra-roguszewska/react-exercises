ec56eb0764fa204951f03237099af7ca637651eb
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-06-05 21:35:24 +0200

    Get data from firestore

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,9 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
 import { db } from "./api/firebase";
-
-console.log(db);
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const newCollection = collection(db, "papraki");
+  console.log(db);
+
+  const getData = () => {
+    getDocs(newCollection).then((dataSnapshot) =>
+      dataSnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+      })
+    );
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
       <p>Hello</p>
