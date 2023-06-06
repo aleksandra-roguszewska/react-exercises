19670f5f0fa7ec67c7df0e4429a1138990469623
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-06-06 11:24:31 +0200

    Display actors data

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,27 +1,75 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { db } from "./api/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
 function App() {
-  const newCollection = collection(db, "papraki");
-  console.log(db);
+  const [actors, setActors] = useState([]);
 
-  const getData = () => {
-    getDocs(newCollection).then((dataSnapshot) =>
-      dataSnapshot.forEach((doc) => {
-        console.log(doc.id, " => ", doc.data());
-      })
-    );
+  const getActors = () => {
+    const actorsCollection = collection(db, "papraki");
+
+    getDocs(actorsCollection).then((querySnapshot) => {
+      const actors = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setActors(actors);
+    });
   };
 
   useEffect(() => {
-    getData();
+    getActors();
+    console.log(actors);
   }, []);
+
+  const onFormSubmit = (event) => {
+    const actorCollection = collection(db, "papraki");
+    const actor = {};
+    event.preventDefault();
+    console.log("Submitted!");
+    actor.firstName = event.target.firstName.value;
+    actor.lastName = event.target.lastName.value;
+    actor.age = event.target.age.value;
+    console.log(actor);
+    event.target.reset();
+    addDoc(actorCollection, actor);
+  };
 
   return (
     <>
-      <p>Hello</p>
+      <h2>Existing actors:</h2>
+      <ul>
+        {actors.map((item) => {
+          return (
+            <li className="actor_item" key={item.id}>
+              <p>Imię: {item.firstName}</p>
+              <p>Nazwisko: {item.lastName}</p>
+              <p>Wiek: {item.age}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <h2>Add new actor:</h2>
+      <form onSubmit={onFormSubmit} className="actor_form">
+        <label htmlFor="firstName">Firstname: </label>
+        <input
+          name="firstName"
+          id="firstName"
+          type="text"
+          placeholder="Your firstname"
+        ></input>
+        <label htmlFor="lastName">Lastname: </label>
+        <input
+          name="lastName"
+          id="lastName"
+          type="text"
+          placeholder="Your lastname"
+        ></input>
+        <label htmlFor="age">Age: </label>
+        <input name="age" id="age" type="number" placeholder="Your age"></input>
+        <button>Submit</button>
+      </form>
     </>
   );
 }
