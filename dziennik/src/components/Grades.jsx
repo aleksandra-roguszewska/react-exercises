98d02409c017537136439adc35f60fb3d2e9c68b
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-06-07 15:29:31 +0200

    Display students grades

--- dziennik/src/components/Grades.jsx
+++ dziennik/src/components/Grades.jsx
@@ -1,30 +1,49 @@
 import { useState, useEffect } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../api/firebase";
+import { Fragment } from "react";
 
 export const Grades = () => {
-  const [grades, setGrades] = useState([]);
+  const [filteredStudentData, setFilteredStudentsData] = useState([]);
   useEffect(() => {
     const collectionRef = collection(db, "students");
     getDocs(collectionRef).then((querySnapShot) => {
-      const gradesData = querySnapShot.docs.map((doc) => ({
+      const filteredData = querySnapShot.docs.map((doc) => ({
         id: doc.id,
-        ...doc.data(),
+        studentName: doc.data().name + " " + doc.data().surname,
+        grades: doc.data().grades,
       }));
-      console.log(gradesData);
-      setGrades(gradesData);
+      console.log(filteredData);
+      setFilteredStudentsData(filteredData);
     });
   }, []);
 
   return (
     <div>
       <h1>Grades</h1>
-      {grades.map((item) => (
-        <ul key={item.id}>
-          <li>{item.name}</li>
-          <li>biologia {item.grades.biologia}</li>
-        </ul>
-      ))}
+      <ul>
+        {filteredStudentData ? (
+          filteredStudentData.map((item) => {
+            return (
+              <li key={item.id}>
+                <p>{item.studentName}</p>
+                {Object.keys(item.grades).map((subject, index) => (
+                  <p key={item.id + index}>
+                    {subject}
+                    <ul>
+                      {item.grades[subject].map((grade, idx) => (
+                        <li key={idx}>{grade}</li>
+                      ))}
+                    </ul>
+                  </p>
+                ))}
+              </li>
+            );
+          })
+        ) : (
+          <p>Błąd</p>
+        )}
+      </ul>
     </div>
   );
 };
