a5a428a3d1451bc0bbe2f64e771a7c766092a3bf
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-06-12 18:13:03 +0200

    add password resetting

--- firebase-actors/02-auth/src/App.jsx
+++ firebase-actors/02-auth/src/App.jsx
@@ -1,5 +1,7 @@
+import { useState, useEffect } from "react";
 import { auth } from "./api/firebase";
-import { Route, Routes, Link } from "react-router-dom";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { Route, Routes, Link, Navigate } from "react-router-dom";
 import Admin from "./components/Admin";
 import Auth from "./components/Auth";
 import ForgotPassword from "./components/ForgotPAssword";
@@ -10,15 +12,57 @@ import Register from "./components/Register";
 console.log(auth);
 
 export const App = () => {
+  const [user, setUser] = useState(null);
+  const [isAuth, setIsAuth] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log("auth status changed", user);
+      if (user) {
+        setIsAuth(true);
+        setUser(user);
+      } else {
+        setIsAuth(false);
+        setUser(null);
+      }
+    });
+  }, []);
+
+  if (isAuth === null) {
+    return <h1>Traw ładowanie aplikacji</h1>;
+  }
+
   return (
     <>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/auth">Panel klienta</Link>
-        <Link to="/auth/register">Register</Link>
-        <Link to="/auth/login">Login</Link>
-        <Link to="/auth/forgot-password">Odzywskiwanie hasła</Link>
-        <Link to="/admin">Panel administratora</Link>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/auth">Panel klienta</Link>
+          </li>
+          <li>
+            <Link to="/auth/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/auth/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/auth/forgot-password">Odzywskiwanie hasła</Link>
+          </li>
+          <li>
+            <Link to="/admin">Panel administratora</Link>
+          </li>
+          <li
+            onClick={() => {
+              signOut(auth);
+              console.log("Wylogowano");
+            }}
+          >
+            Wyloguj się
+          </li>
+        </ul>
       </nav>
 
       <h1>Hello, Firebase Auth!</h1>
@@ -29,7 +73,10 @@ export const App = () => {
           <Route path="login" element={<Login />} />
           <Route path="forgot-password" element={<ForgotPassword />} />
         </Route>
-        <Route path="/admin" element={<Admin />} />
+        <Route
+          path="/admin"
+          element={!isAuth ? <Navigate to="/" /> : <Admin />}
+        />
       </Routes>
     </>
   );
--- firebase-actors/02-auth/src/components/ForgotPassword.jsx
+++ firebase-actors/02-auth/src/components/ForgotPassword.jsx
@@ -1,6 +1,17 @@
+import { auth } from "../api/firebase";
+import { sendPasswordResetEmail } from "firebase/auth";
+
 const ForgotPassword = () => {
+  const handlePasswordReset = (event) => {
+    event.preventDefault();
+    sendPasswordResetEmail(auth, event.target.email.value).catch((e) => {
+      console.log(e.code);
+      // alert(firebaseErrors[e.code]);
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handlePasswordReset}>
       <label htmlFor="email">Email: </label>
       <input id="email" name="email" type="email"></input>
       <button>Odzyskaj hasło</button>
