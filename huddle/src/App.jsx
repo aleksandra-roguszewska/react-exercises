c6a7ad33e942978de79cccbf88a72d1eeb667a38
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-07-18 11:16:57 +0200

    Feat: Complete huddle layout with styled-components

--- huddle/src/App.jsx
+++ huddle/src/App.jsx
@@ -1,7 +1,10 @@
-import Header from "./components/Header";
-import { Container } from "./components/styles/Container.styled";
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
+import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
+import content from "./content";
 
 const theme = {
   colors: {
@@ -9,16 +12,22 @@ const theme = {
     body: "#fff",
     footer: "#003333",
   },
+  mobile: "768px",
 };
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Container>
+      <>
+        <GlobalStyles />
         <Header />
-        <h1>Hello world!</h1>
-      </Container>
+        <Container>
+          {content.map((item, index) => (
+            <Card key={index} item={item} />
+          ))}
+        </Container>
+        <Footer />
+      </>
     </ThemeProvider>
   );
 }
